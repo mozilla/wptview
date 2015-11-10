2e5ec0e6ce8494e8e207f6fe96acfca55e819edc
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-11-10 15:34:23 +0530

    fixing changes

--- logcruncher.js
+++ logcruncher.js
@@ -1,14 +1,11 @@
-function LogCruncher(rawtext) {
-	this.JSONArray = [];
+function logCruncher(rawtext) {
+	var JSONArray = [];
 	var lines = rawtext.split('\n');
 	for (var i=0; i<lines.length; i++) {
 		if (lines[i]=="")
 			continue;
 		var json = JSON.parse(lines[i]);
-		this.JSONArray.push(json);
+		JSONArray.push(json);
 	}
+	return JSONArray;
 }
-
-LogCruncher.prototype = {
-	
-};
