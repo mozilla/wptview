dddb3935e9ac5b002947b41fb2ef45aa6e40c233
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-11-10 22:05:05 +0530

    adding log filter function

--- logcruncher.js
+++ logcruncher.js
@@ -6,7 +6,40 @@ function logCruncher(rawtext) {
       continue;
     }
     var json = JSON.parse(lines[i]);
-    JSONArray.push(json);
+    if (filterOutput(json)) {
+      JSONArray.push(json);
+    }
   }
+  console.log(lines.length+", ");
   return JSONArray;
+}
+
+function logParameter(parameter, valueRegex) {
+  this.parameter = parameter;
+  this.valueRegex = valueRegex;
+}
+logParameter.prototype = {
+
+}
+
+function filterOutput(parsedLine) {
+  var isValidOutput = false;
+  var validTestEntries = [
+    [ new logParameter("action", /^test/) ],
+    [ new logParameter("action", /^log/), new logParameter("level", /^(error)|(critical)/i) ],
+  ];
+  validTestEntries.forEach(function(validTestEntry) {
+    var isValidTestEntry = true;
+    validTestEntry.forEach(function(logParameter) {
+      var pattr = logParameter.valueRegex;
+      var result = pattr.test(parsedLine[logParameter.parameter]);
+      if (!result) {
+        isValidTestEntry = false;
+      }
+    });
+    if (isValidTestEntry) {
+      isValidOutput = true;
+    }
+  });
+  return isValidOutput;
 }
