42df2b9a5ed0064e763d13a7310eb4e1a0ed3d35
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-11-14 01:08:00 +0530

    Adding test_results table to IndexedDB. r=jgraham

--- LovefieldService.js
+++ LovefieldService.js
@@ -2,6 +2,7 @@ var LovefieldService = function() {
   // Following member variables are initialized within getDbConnection().
   this.db_ = null;
   this.tests = null;
+  this.test_results = null;
 };
 
 /**
@@ -11,6 +12,7 @@ var LovefieldService = function() {
  */
 LovefieldService.prototype.onConnected_ = function() {
   this.tests = this.db_.getSchema().table('tests');
+  this.test_results = this.db_.getSchema().table('test_results');
 };
 
 
@@ -41,8 +43,18 @@ LovefieldService.prototype.buildSchema_ = function() {
   var schemaBuilder = lf.schema.create('wptview', 1);
   schemaBuilder.createTable('tests').
       addColumn('id', lf.Type.INTEGER).
+      addColumn('test', lf.Type.STRING).
+      addPrimaryKey(['id'], true);
+  schemaBuilder.createTable('test_results').
+      addColumn('result_id', lf.Type.INTEGER).
+      addColumn('status', lf.Type.STRING).
+      addColumn('message', lf.Type.STRING).
       addColumn('test_id', lf.Type.INTEGER).
-      addPrimaryKey(['id'],true);
+      addPrimaryKey(['result_id'], true).
+      addForeignKey('fk_test_id', {
+        local: 'test_id',
+        ref: 'tests.id'
+      });
 
   return schemaBuilder;
 };
@@ -51,20 +63,46 @@ LovefieldService.prototype.buildSchema_ = function() {
 var testLogsRaw;
 
 
-LovefieldService.prototype.insertData = function(testLogsRaw) {
+LovefieldService.prototype.insertTests = function(testLogsRaw) {
   var testRows = [];
   var tests = this.tests;
   testLogsRaw.forEach(function(testLog) {
     if (testLog.action == "test_start") {
       var row = tests.createRow({
-        'test_id': testLog.test
+        'test': testLog.test
       });
       testRows.push(row);
     }
   });
   var q1 = this.db_.
       insert().
-      into(this.tests).
+      into(tests).
       values(testRows);
   return q1.exec();
 };
+
+LovefieldService.prototype.insertTestResults = function(testLogsRaw, tests) {
+  // Let's first create a test to id mapping
+  testIds = {};
+  tests.forEach(function(test) {
+    testIds[test.test] = test.id;
+  });
+  var testResultsRows = [];
+  var test_results = this.test_results;
+  testLogsRaw.forEach(function(testLog) {
+    if (testLog.action == "test_end") {
+      var resultId = testIds[testLog.test];
+      var row = test_results.createRow({
+        'status': testLog.status,
+        'message': testLog.message,
+        'test_id': resultId
+      });
+      testResultsRows.push(row);
+    }
+  });
+  var q1 = this.db_.
+      insert().
+      into(test_results).
+      values(testResultsRows);
+  return q1.exec();
+}
