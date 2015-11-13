8ad1728768e1f8f384729f63e40ec42b82b8b8f3
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-11-14 01:08:00 +0530

    Adding test_results table to IndexedDB

--- LovefieldService.js
+++ LovefieldService.js
@@ -2,6 +2,7 @@ var LovefieldService = function() {
   // Following member variables are initialized within getDbConnection().
   this.db_ = null;
   this.tests = null;
+  this.test_results = null;
 };
 
 /**
@@ -11,6 +12,7 @@ var LovefieldService = function() {
  */
 LovefieldService.prototype.onConnected_ = function() {
   this.tests = this.db_.getSchema().table('tests');
+  this.test_results = this.db_.getSchema().table('test_results');
 };
 
 
@@ -41,8 +43,19 @@ LovefieldService.prototype.buildSchema_ = function() {
   var schemaBuilder = lf.schema.create('wptview', 1);
   schemaBuilder.createTable('tests').
       addColumn('id', lf.Type.INTEGER).
-      addColumn('test_id', lf.Type.INTEGER).
+      addColumn('test_file', lf.Type.STRING).
       addPrimaryKey(['id'],true);
+  schemaBuilder.createTable('test_results').
+      addColumn('result_id', lf.Type.INTEGER).
+      addColumn('status', lf.Type.STRING).
+      addColumn('message', lf.Type.STRING).
+      addColumn('test_file', lf.Type.STRING).
+      addColumn('test_id', lf.Type.INTEGER).
+      addPrimaryKey(['result_id'],true).
+      addForeignKey('fk_test_id', {
+        local: 'test_id',
+        ref: 'tests.id'
+      });
 
   return schemaBuilder;
 };
@@ -53,18 +66,36 @@ var testLogsRaw;
 
 LovefieldService.prototype.insertData = function(testLogsRaw) {
   var testRows = [];
+  var testResultsRows = [];
   var tests = this.tests;
+  var test_results = this.test_results;
+  var testId = 0;
   testLogsRaw.forEach(function(testLog) {
     if (testLog.action == "test_start") {
       var row = tests.createRow({
-        'test_id': testLog.test
+        'test_file': testLog.test
       });
       testRows.push(row);
+      testId = testId + 1;
+    }
+    else if (testLog.action == "test_end") {
+      var row = test_results.createRow({
+        'status': testLog.status,
+        'message': testLog.message,
+        'test_file': testLog.test,
+        'test_id': testId
+      });
+      testResultsRows.push(row);
     }
   });
   var q1 = this.db_.
       insert().
       into(this.tests).
       values(testRows);
-  return q1.exec();
+  var q2 = this.db_.
+      insert().
+      into(this.test_results).
+      values(testResultsRows);
+  var tx = this.db_.createTransaction();        
+  return tx.exec([q1, q2]);
 };
