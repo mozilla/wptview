0ec20c7d6698b14d33c588dd9d436ab523f6ffad
Akash Trehan <akash.trehan123@gmail.com>
2016-01-01 19:13:11 +0530

    logcruncher.js == changed to ===

--- logcruncher.js
+++ logcruncher.js
@@ -14,7 +14,7 @@ function logCruncher(rawtext, filter) {
     var JSONArray = [];
     var lines = rawtext.split('\n');
     for (var i=0; i<lines.length; i++) {
-      if (lines[i]=="") {
+      if (lines[i]==="") {
         continue;
       }
       var json = JSON.parse(lines[i]);
@@ -29,7 +29,7 @@ function logCruncher(rawtext, filter) {
 function testsFilter(parsedLine) {
   var pattr = /^test_/;
   var pattr2 = /^(?:error)|(?:critical)/i;
-  return (pattr.test(parsedLine.action) || (parsedLine.action=="log" && pattr2.test(parsedLine.level)));
+  return (pattr.test(parsedLine.action) || (parsedLine.action==="log" && pattr2.test(parsedLine.level)));
 }
 
 function readFile(file) {
