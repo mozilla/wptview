80b3adad9f5693e17218c4966da91cd3c936f979
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-03-24 03:06:11 +0530

    adding travis script

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -1,1 +1,17 @@
-echo "Testing";
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Kalpesh Krishna"
+git config user.email "kalpeshk2011@gmail.com"
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy to GitHub Pages".
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+# Force push from the current repo's master branch to the remote
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
