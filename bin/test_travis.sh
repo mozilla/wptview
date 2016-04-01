e469efdaf00de56a470b715505a47758ed25f542
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-04-01 19:53:38 +0530

    Fixing travis script, changing error code

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
+echo $TRAVIS_PULL_REQUEST
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-	exit 1
+	exit 0
 fi
-git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+echo "Pushing to gh-pages.."
+git push "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
