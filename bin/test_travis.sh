9ee0045f30bb75384ab5f7d7db07c5f06c7ee073
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-04-01 19:50:00 +0530

    Changing travis script exit syntax

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-	exit
+	exit 1
 fi
 git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
