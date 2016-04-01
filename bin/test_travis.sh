92797a875c700da196a64ca5936bc0c432777812
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-04-01 20:44:02 +0530

    Adding a new secure key

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -5,5 +5,5 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 	exit 0
 fi
 echo "Pushing to gh-pages.."
-git push --progress "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 echo "Finished pushing to gh-pages.."
